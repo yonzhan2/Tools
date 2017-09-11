729ee151ab2219333c056f7495a3530cbde622e5
yonzhan2 <yonzhan2@cisco.com>
2017-09-11 12:56:59 +0800

    updated new code

--- getversion.py
+++ getversion.py
@@ -2,7 +2,7 @@
 
 __author__ = 'YongZhang'
 __version__ = '1.0.0'
-#coding:utf-8
+# coding:utf-8
 import paramiko
 import os
 import socket
@@ -10,103 +10,80 @@ import time
 import json
 import difflib
 import shutil
-
-serverlists = {'J2ee': ['10.224.89.100'],'DPL':['10.224.89.211'],'MJS':['10.224.89.77'],'MRS':['10.224.89.186'],'Reminder':['10.224.89.101'],'AS':['10.224.89.39'],
-               \
-               'Superadmin': ['10.224.82.69'], 'CB': ['10.224.89.243'],  'WWP':['10.224.89.224'], 'RA':['10.224.89.240'],
-               \
-               'MMPMCC':['10.224.82.96'],'MMPMCS':['10.224.82.98'],'NBRDPS':['10.224.82.73'],'NBRVSS':['10.224.82.74'],'NBRWES':['10.224.82.72'],
-               \
-               'NBRWSS':['10.224.82.242'],'NBRMSC':['10.224.82.85'],'NBRMSS':['10.224.82.87'],'TSP':['10.224.54.164'],
-               \
-               'TahoeTS':['10.224.82.77'],'TahoeTAS':['10.224.82.79'],'TahoeMACC':['10.224.82.83'],'TPGW':['10.224.89.241'],'CMS':['10.224.82.40'],
-               \
-               'XMLAPI':['10.224.89.225'],'GLA':['10.224.82.70'],'WebACD':['10.224.82.100'],'ACDBRE':['10.224.82.101']
-               }
-servertypes = ['J2ee','DPL','MJS','MRS','Reminder','AS', 'Superadmin', 'CB','WWP','RA','MMPMCC','MMPMCS','NBRDPS','NBRVSS','NBRWES','NBRMSC','NBRMSS','NBRWSS',
-               \
-               'TahoeTS','TahoeMACC','TahoeTAS' ,'TPGW','CMS','TSP','XMLAPI','GLA','WebACD','ACDBRE']
+import ConfigParser
+import requests
+import telnetlib
+import re
 
 new_html = '/www/htdocs/versionlist.html'
 old_html = '/www/htdocs/versionlist_old.html'
 diff_html = '/www/htdocs/diff.html'
 json_file = '/tmp/pkgdict.json'
-old_json_file = '/tmp/pkgdict_old.json'
-report_date = time.strftime('%m/%d/%Y %H:%M:%S GMT',time.localtime(time.time()))
-backup_date = time.strftime('%Y%m%d%H',time.localtime(time.time()))
+base_jsonfile = '/tmp/pkgdict_base.json'
+base_jsonfile_tmp = '/tmp/pkgdict_base_tmp.json'
+report_date = time.strftime('%m/%d/%Y %H:%M:%S GMT', time.localtime(time.time()))
+backup_date = time.strftime('%Y%m%d%H', time.localtime(time.time()))
 backup_file = '/www/htdocs/history/versionlist_{0}.html'.format(backup_date)
+cur_hour = time.localtime().tm_hour
+_lock = '/tmp/.lock'
 
-PkgDict={}
+PkgDict = {}
+status = {}
+rpmcmd = {}
 
-def GetpkgInfo(type,server):
-    if type == 'J2ee':
-        command = 'sudo rpm -qa | grep -e WBXpagecommon -e WBXpage -e WBXadmin'
-    elif type == 'DPL':
-        command = 'sudo rpm -qa | grep -e static -e WBXclient.T3 -e WBXmsi.T3 -e WBXptool -e mjs'
-    elif type == 'MJS':
-        command = 'sudo rpm -qa | grep mjs'
-    elif type == 'MRS':
-        command = 'sudo rpm -qa | grep -e mrs -e mbs'
-    elif type == 'Reminder':
-        command = 'sudo rpm -qa | grep -e WBXreminder -e pagecommon -e logadmin'
-    elif type == 'AS':
-        command = 'sudo rpm -qa | grep WBXauth'
-    elif type == 'Superadmin':
-        command = 'sudo rpm -qa | grep WBXsuper'
-    elif type == 'CB':
-        command = 'sudo rpm -qa | grep WBXerk'
-    elif type == 'WWP':
-        command = 'sudo rpm -qa | grep WBXerk'
-    elif type == 'RA':
-        command = 'sudo rpm -qa | grep WBXerk'
-    elif type == 'MMPMCC':
-        command = 'sudo rpm -qa | grep WBXmcc'
-    elif type == 'MMPMCS':
-        command = 'sudo rpm -qa | grep WBXmcs'
-    elif type == 'NBRDPS':
-        command = 'sudo rpm -qa | grep WBXnbr'
-    elif type == 'NBRVSS':
-        command = 'sudo rpm -qa | grep WBXnbr'
-    elif type == 'NBRWES':
-        command = 'sudo rpm -qa | grep WBXnbr'
-    elif type == 'NBRMSC':
-        command = 'sudo rpm -qa | grep WBXmsc'
-    elif type == 'NBRMSS':
-        command = 'sudo rpm -qa | grep WBXmss'
-    elif type == 'NBRWSS':
-        command = 'sudo rpm -qa | grep -e WBXnbrwss -e pagecommon'
-    elif type == 'TahoeTS':
-        command = 'sudo rpm -qa | grep -e WBXjtel -e WBXmacc -e WBXtahoeas'
-    elif type == 'TahoeMACC':
-        command = 'sudo rpm -qa | grep -e WBXjtel -e WBXmacc -e WBXtahoeas'
-    elif type == 'TahoeTAS':
-        command = 'sudo rpm -qa | grep -e WBXjtel -e WBXmacc -e WBXtahoeas'
-    elif type == 'TPGW':
-        command = 'sudo rpm -qa | grep WBXtpgw'
-    elif type == 'CMS':
-        command = 'sudo rpm -qa | grep -e WBXcms -e WBXaudioclips'
-    elif type == 'TSP':
-        command = 'sudo rpm -qa | grep -e WBXpai'
-    elif type == 'XMLAPI':
-        command = 'sudo rpm -qa | grep -e WBXsxa -e WBXxmlapi.11 -e WBXxmlproxy'
-    elif type == 'GLA':
-        command = 'sudo rpm -qa | grep -e WBXgloballookupapi'
-    elif type == 'WebACD':
-        command = 'sudo rpm -qa | grep -e WBXacd '
-    elif type == 'ACDBRE':
-        command = 'sudo rpm -qa | grep -e WBXacd '
+cf = ConfigParser.ConfigParser()
+cf.read(os.path.dirname(os.path.abspath(__file__)) + os.sep + 'hckconfig.properties')
+secs = cf.sections()
 
 
+# print secs
+
+
+class HealthCheck:
+    "get the server's health check status via the parameter input"
+
+    def __init__(self, type):
+        self.type = type
+        self.ip, self.rpmcmd, self.hckurl = (cf.get(type, opt) for opt in cf.options(type))
+        rpmcmd[type] = self.rpmcmd
+
+    def getStatus(self):
+        try:
+            data = requests.get(self.hckurl, timeout=10)
+            m_okokok = re.compile('OKOKOK', re.I)
+            m_online = re.compile('online', re.I)
+            if m_okokok.search(data.content) or m_online.search(data.content):
+                status[self.type] = 'OKOKOK'
+                return "OKOKOK"
+            else:
+                status[self.type] = 'NONONO'
+                return 'NONONO'
+        except:
+            status[self.type] = 'NONONO'
+            return 'NONONO'
+
+    def Telnet(self):
+        try:
+            tn = telnetlib.Telnet(self.ip, self.hckurl)
+            status[self.type] = 'OKOKOK'
+            return 'OKOKOK'
+        except:
+            status[self.type] = 'NONONO'
+            return 'NONONO'
+
+
+def GetpkgInfo(type, server):
+    'Get Package info via the input paramter server type and server ip, return Package Info Dict'
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     hostname = ""
     ipaddr = ""
-    for user, pwd in zip(['wbxbuilds'], ['P0w3rSupply!']): #['logs', 'wbxbuilds'], ['wbx@Aalogs', 'P0w3rSupply!'
-        #print user, pwd
+    for user, pwd in zip(['wbxbuilds'], ['P0w3rSupply!']):  # ['logs', 'wbxbuilds'], ['wbx@Aalogs', 'P0w3rSupply!'
+        # print user, pwd
         try:
-            ssh.connect(server, username=user, password=pwd,timeout=30)
-            stdin, stdout, stderr = ssh.exec_command(command)
+            ssh.connect(server, username=user, password=pwd, timeout=30)
+            stdin, stdout, stderr = ssh.exec_command(rpmcmd[type])
             hostname = socket.gethostbyaddr(server)[0]
             ipaddr = socket.gethostbyname(hostname)
             print hostname, ipaddr
@@ -114,126 +91,164 @@ def GetpkgInfo(type,server):
         except:
             pass
 
-    PkgDict[type]={"build":[line.strip() for line in sorted(stdout.readlines())],"hostname":hostname,"ipaddr":ipaddr}
-    #print PkgDict
+    # p1 = re.compile(r"Source RPM: (.+?) Size :") ##Source RPM Name
+    p1 = re.compile(r"Name : (.+?) Relocations:(.+?) Version :(.+?) Vendor: \(none\) Release : (.+?) Build Date")
+    p2 = re.compile(r"Build Date: (.+?) Install Date:")  ##Build Date
+    p3 = re.compile(r"Install Date: (.+?) Build Host:")  ##Install Date
+    with open('/tmp/stdout', 'w+') as f:
+        f.write(stdout.read())
+    with open('/tmp/stdout') as fh:
+        f = fh.read()
+    # print "content of f:", f
+    # print re.findall(p1, f)
+    p1_ret = re.findall(p1, f)
+    print 'p1 :', p1_ret
+    buildsinfo = ['{0}-{1}-{2}'.format(build[0].strip(), build[2].strip(), build[3].strip()) for build in p1_ret]
+    # print re.findall(p2, f)
+    # print re.findall(p3, f)
+    rpmdata = zip(buildsinfo, re.findall(p2, f), re.findall(p3, f))
+    print rpmdata
+
+    # PkgDict[type]={"build":[line.strip() for line in sorted(stdout.readlines())],"hostname":hostname,"ipaddr":ipaddr}
+    PkgDict[type] = {"build": [line for line in sorted(rpmdata)], "hostname": hostname, "ipaddr": ipaddr}
+    print PkgDict
 
 
-
-
-
-def getDataFromJson(type,jsonfile=json_file):
-    with open (jsonfile) as json_data:
-        data = json.load (json_data)
+def getDataFromJson(type, jsonfile=json_file):
+    'Get data from jsonfile, return tuple'
+    with open(jsonfile) as json_data:
+        data = json.load(json_data)
         datadict = data[type]
-        #print datadict
-        #print datadict["hostname"], datadict["ipaddr"], datadict["build"]
-        #print "rows are ", len (datadict["build"])
-        rows = len (datadict["build"])
+        # print datadict
+        rows = len(datadict["build"])
         hostname = datadict["hostname"]
         ipaddr = datadict["ipaddr"]
         build = datadict["build"]
-        return rows,hostname,ipaddr,build
+
+        return rows, hostname, ipaddr, build
 
 
-
+def timeConvert(s):
+    timestruct = time.strptime(s, "%a %b %d %H:%M:%S %Y")
+    timeformat = time.strftime("%Y-%m-%d %H:%M:%S", timestruct)
+    timestamp = time.mktime(timestruct)
+    return timeformat, timestamp
 
 
 def writeHtmlHeader():
-
     if os.path.isfile(new_html):
-        shutil.copy(new_html,backup_file)
-        os.rename(new_html,old_html)
-        d=os.popen('find /www/htdocs/history -mtime +5 -exec rm -f {} \;')
+        try:
+            shutil.copy(new_html, backup_file)
+            # os.rename(new_html,old_html)
+            d = os.popen('find /www/htdocs/history -mtime +5 -exec rm -f {} \;')
+        except:
+            pass
 
-    with open(new_html,'a+') as html:
+    with open(new_html, 'a+') as html:
         html.write(
-'''<html>
-</style>
-<head>
-<title>Package Version Check List</title>
-</head>
-<body>
-  <h1 style="font-style:italic">Package Version Check List on hf3wd</h1>
-  <tr>
-    <td>Report Date: {0} </td>
-  </tr>
-  <br>
-   <a href="http://pvc.qa.webex.com/diff.html" target="_blank">Package Diff</a>
-  </br>
-<table border="1">
-  <tr>
-    <th>Server Type</th>
-    <th>Hostname</th>
-    <th>IP Address</th>
-    <th>Build Version</th>
-  </tr>
-  '''.format(report_date)
-)
+            r'''<!DOCTYPE html>
+            <html>
+            </style>
+            <head>
+            <title>Package Version Check List</title>
+            </head>
+            <body>
+              <h1 style="font-style:italic">Package Version Check List on hf3wd</h1>
+              <li>Current Location: {0} </li>
+              <li>Report Date: {1} </li>
+              <br>
+               <a href="http://pvc.qa.webex.com/diff.html" target="_blank">Package Diff</a>
+              </br>
+            <table border="1">
+              <tr>
+                <th>Server Type</th>
+                <th>Hostname</th>
+                <th>IP Address</th>
+                <th>Build Date</th>
+                <th>Deploy Date</th>
+                <th>Build Version</th>
+                <th>Service Status</th>
+              </tr>
+              '''.format('PRIMARY' if status.get('J2EE', 'NONONO') == 'OKOKOK' else 'GSB', report_date)
+        )
 
 
 def writeHtmlBody(type):
-
     rows = getDataFromJson(type)[0]
     hostname = getDataFromJson(type)[1]
     ipaddr = getDataFromJson(type)[2]
-    build = getDataFromJson(type)[3]
-    color = {0:'''bgcolor=""''',1:'''bgcolor="#50D050"'''}
+    builds = getDataFromJson(type)[3]
+    color = {0: '''bgcolor=""''', 1: '''bgcolor="#50D050"''', 2: '''bgcolor="#FF0000"'''}
 
     def isChanged(build):
-        old_build = getDataFromJson (type, old_json_file)[3]
-        #print "old_build is", old_build
+        old_build = (build[0] for build in getDataFromJson(type, base_jsonfile)[3])
+        # print "getdatafromjson is ",getDataFromJson (type, base_jsonfile)[3]
+        # print "old_build is", old_build, "new build is" ,build
         if build not in old_build:
             return color[1]
         else:
             return color[0]
 
+    def isRunning(type):
+        if status.get(type, '') == 'OKOKOK' or status.get(type, '') == '':
+            return color[0]
+        else:
+            return color[2]
+
+    'if not get the build version, return empty list'
 
     try:
-        firstbuild = build[0]
+        firstbuild = builds[0][0]
+        # print "firstbuild is ",firstbuild
     except:
         firstbuild = []
 
-
-    with open (new_html, 'a+') as html:
-        html.write (
-'''
-<tr>
-    <td rowspan="{0}" align="center">{1}</td>
-    <td rowspan="{0}" align="center">{2}</td>
-    <td rowspan="{0}" align="center">{3}</td>
-    <td {4}>{5}</td>
- </tr>
-
-'''.format(rows,type,hostname,ipaddr,isChanged(firstbuild),firstbuild)
-)
-
-    if len(build) > 1:
-        for build in build[1:]:
-            with open(new_html,'a+') as html:
+    with open(new_html, 'a+') as html:
+        html.write(
+            '''
+            <tr>
+                <td rowspan="{0}" align="center">{1}</td>
+                <td rowspan="{0}" align="center">{2}</td>
+                <td rowspan="{0}" align="center">{3}</td>
+                <td >{8}</td>
+                <td >{9}</td>
+                <td {4}>{5}</td>
+                <td rowspan="{0}" align="center" {6}>{7}</td>
+             </tr>
+            
+            '''.format(rows, type, hostname, ipaddr, isChanged(firstbuild), firstbuild, isRunning(type),
+                       status.get(type, ''),
+                       timeConvert(builds[0][1])[0], timeConvert(builds[0][2])[0])
+        )
+    'If the length of build >1, write to file from the second entity'
+    if len(builds) > 1:
+        for build in builds[1:]:
+            # print "build is" ,build
+            with open(new_html, 'a+') as html:
                 html.write(
-'''
-  <tr>
-    <td {0} >{1}</td>
-  </tr>
-'''.format(isChanged(build),build)
-)
-
+                    '''
+                      <tr>
+                        <td >{0}</td>
+                        <td >{1}</td>
+                        <td {2} >{3}</td>
+                      </tr>
+                    '''.format(timeConvert(build[1])[0], timeConvert(build[2])[0], isChanged(build[0]), build[0])
+                )
 
 
 def writeHtmlTail():
-
-    with open(new_html,'a+') as html:
+    with open(new_html, 'a+') as html:
         html.write(
-'''
-</table>
-</body>
-</html>
-''')
+            '''
+            </table>
+            </body>
+            </html>
+            ''')
 
 
-def diffHtml(new,old):
+def diffHtml(new, old):
     def readfile(filename):
-        with open(filename,'rb') as f:
+        with open(filename, 'rb') as f:
             text = f.read().splitlines()
         return text
 
@@ -241,34 +256,85 @@ def diffHtml(new,old):
     text2_lines = readfile(old)
 
     d = difflib.HtmlDiff()
-    diff = d.make_file(text1_lines,text2_lines)
+    diff = d.make_file(text1_lines, text2_lines)
 
-    with open(diff_html,'w+') as f:
+    with open(diff_html, 'w+') as f:
         f.write(diff)
 
 
+def writeHtml():
+    writeHtmlHeader()
+    for type in secs:
+        writeHtmlBody(type)
+    writeHtmlTail()
+
+
+def transferToCI():
+    f = open(json_file)
+    data = json.load(f)
+    cnvt = [{'buildDate': timeConvert(b[1])[1], 'deployDate': timeConvert(b[2])[1], 'name': b[0]} for i in data.values()
+            for b in i['build']]
+    ret = json.dumps(cnvt, sort_keys=True, indent=4, separators=(',', ': '))
+    # print 'Transfer content:',ret
+
+    url = 'http://10.224.57.221:8090/data/v1/buildDeployHistory/QA/packageList?appKey=qatest&appSecret=CntbrbG-aSTCHdYe3Pa8NrT7XtlMGEKV2NSlD9B_Xmk'
+    headers = {'Content-Type': 'application/json;charset=UTF-8'}
+    try:
+        r = requests.post(url, headers=headers, data=ret)
+        print 'response staus is', r.status_code
+        print 'response content is', r.content
+    except:
+        print 'call api failed'
+
+
 if __name__ == "__main__":
+    for type in secs:
+        hck = HealthCheck(type)
+        # print hck.ip,hck.rpmcmd,hck.hckurl
+        print type, hck.ip, hck.getStatus()
+    # print 'url status is %s' % status
 
+    for type in 'DPL', 'RA', 'TahoeTS', 'WebACD', 'TSP':
+        hck = HealthCheck(type)
+        # print hck.ip,hck.rpmcmd,hck.hckurl
+        print type, hck.ip, hck.Telnet()
+    print 'status is %s' % status
 
+    for type in secs:
+        server = cf.get(type, 'ip')
+        # print server
+        GetpkgInfo(type, server)
 
-    for type,serverlist in serverlists.iteritems():
-        print type,serverlist[0]
-        server=serverlist[0]
-        GetpkgInfo (type, server)
+    PkgDict_json = json.dumps(PkgDict, sort_keys=True, indent=4, separators=(',', ': '))
+    # print PkgDict_json
 
-    if os.path.isfile (json_file):
-        os.rename (json_file, old_json_file)
-
-    PkgDict_json = json.dumps(PkgDict,sort_keys=True, indent=4, separators=(',', ': '))
-    print PkgDict_json
-
-    with open (json_file, 'a+') as f:
+    with open(json_file, 'w+') as f:
         f.write(PkgDict_json)
 
+    if os.path.isfile(json_file) and not os.path.isfile(base_jsonfile) and not os.path.isfile(base_jsonfile_tmp):
+        shutil.copy(json_file, base_jsonfile)
+        shutil.copy(json_file, base_jsonfile_tmp)
 
-    writeHtmlHeader ()
-    for type in servertypes:
-        writeHtmlBody (type)
-    writeHtmlTail ()
 
-    diffHtml(new_html,old_html)
+    def copyfile(source, destination):
+        if not os.path.isfile(_lock):
+            try:
+                shutil.copy(source, destination)
+                os.system('touch ' + _lock)
+            except:
+                pass
+
+
+    if cur_hour == 0:
+        copyfile(base_jsonfile_tmp, base_jsonfile)
+    elif cur_hour == 5:
+        copyfile(json_file, base_jsonfile_tmp)
+    else:
+        os.system('rm -f ' + _lock)
+
+    ###writehtml
+    writeHtml()
+
+    diffHtml(new_html, old_html)
+
+    transferToCI()
